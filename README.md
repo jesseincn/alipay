d7e6dd49c32f941be804be36013f2a10a5dae4d0
Kerlin Huang <ikerlin@msn.com>
2018-06-15 10:57:37 +0800

    实现支付宝生活号网页授权

--- README.md
+++ README.md
@@ -11,22 +11,24 @@
 
 3. `app/Providers/EventServiceProvider.php` 中添加一个监听器：`SocialiteProviders\Manager\SocialiteWasCalled`，如果已存在则忽略；并添加监听响应事件：`Ikerlin\Alipay\AlipayExtendSocialite@handle`
 
-	```
-	'SocialiteProviders\Manager\SocialiteWasCalled' => [
-		// ...
-		'Ikerlin\Alipay\AlipayExtendSocialite@handle',
-	],
-	```
+    ```
+    'SocialiteProviders\Manager\SocialiteWasCalled' => [
+        // ...
+        'Ikerlin\Alipay\AlipayExtendSocialite@handle',
+    ],
+    ```
 
 4. config/service.php 中添加一个配置项：
 
-	```
-	'alipay' => [
-		'client_id'     => env('ALIPAY_KEY'),
-		'client_secret' => env('ALIPAY_SECRET'),
-		'redirect'      => env('ALIPAY_REDIRECT_URL'),
-	],
-	```
+    ```
+    'alipay' => [
+        'client_id'     => env('ALIPAY_KEY'),
+        'client_secret' => env('ALIPAY_SECRET'), //未使用参数，但不可移除
+        'privateKeyFilePath' => env('ALIPAY_PRIVATE_KEY_FILE_PATH'), //相对于storage目录
+        'publicKeyFilePath' => env('ALIPAY_PUBLIC_KEY_FILE_PATH'), //相对于storage目录
+        'redirect'      => env('ALIPAY_REDIRECT_URL'),
+    ],
+    ```
 
 ## Usage
 
--- src/Provider.php
+++ src/Provider.php
@@ -2,9 +2,15 @@
 
 namespace Ikerlin\Alipay;
 
-use Laravel\Socialite\Two\ProviderInterface;
-use SocialiteProviders\Manager\OAuth2\AbstractProvider;
 use SocialiteProviders\Manager\OAuth2\User;
+use Laravel\Socialite\Two\ProviderInterface;
+use GuzzleHttp\ClientInterface;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+
+use Ikerlin\Alipay\Aop\AopClient;
+use Ikerlin\Alipay\Aop\Request\AlipaySystemOauthTokenRequest;
+use Ikerlin\Alipay\Aop\Request\AlipayUserUserinfoShareRequest;
+use Illuminate\Support\Facades\Storage;
 
 class Provider extends AbstractProvider implements ProviderInterface
 {
@@ -16,9 +22,41 @@ class Provider extends AbstractProvider implements ProviderInterface
     /**
      * {@inheritdoc}.
      */
+    protected $scopes = ['auth_userinfo'];
+
+    /**
+     * @var string
+     */
+    protected $userId;
+
+    /**
+     * {@inheritdoc}.
+     */
     protected function getAuthUrl($state)
     {
-        return $this->buildAuthUrlFromBase('https://openapi.alipay.com/gateway.do', $state);
+        return $this->buildAuthUrlFromBase('https://openauth.alipay.com/oauth2/publicAppAuthorize.htm', $state);
+    }
+
+    /**
+     * {@inheritdoc}.
+     */
+    protected function buildAuthUrlFromBase($url, $state)
+    {
+        $query = http_build_query($this->getCodeFields($state), '', '&', $this->encodingType);
+
+        return $url . '?' . $query;
+    }
+
+    /**
+     * {@inheritdoc}.
+     */
+    protected function getCodeFields($state = null)
+    {
+        return [
+            'app_id' => $this->clientId, 'redirect_uri' => $this->redirectUrl,
+            'scope' => $this->formatScopes($this->scopes, $this->scopeSeparator),
+            'state' => $state,
+        ];
     }
 
     /**
@@ -34,12 +72,16 @@ class Provider extends AbstractProvider implements ProviderInterface
      */
     protected function getUserByToken($token)
     {
-        $response = $this->getHttpClient()->get('https://openapi.alipay.com/gateway.do', [
-            'query' => [
-                'access_token' => $token,
-            ],
-        ]);
-        return json_decode($response->getBody()->getContents(), true);
+        if (in_array('auth_base', $this->scopes)) {
+            $user = ['alipay_user_id' => $this->userId];
+        } else {
+            $alipayUserUserinfoShareRequest = new AlipayUserUserinfoShareRequest();
+            $result = $this->executeAopClient($alipayUserUserinfoShareRequest, $token);
+
+            $user = objectToArray($result->alipay_user_userinfo_share_response);
+        }
+
+        return $user;
     }
 
     /**
@@ -48,37 +90,86 @@ class Provider extends AbstractProvider implements ProviderInterface
     protected function mapUserToObject(array $user)
     {
         return (new User())->setRaw($user)->map([
-            'id' => $user['userid'],
-            'nickname' => $user['username'],
-            'avatar' => $this->getPortraitUrl($user['portrait']),
+            'id' => $user['alipay_user_id'],
+            'nickname' => isset($user['nick_name']) ? $user['nick_name'] : null,
+            'avatar' => isset($user['avatar']) ? $user['avatar'] : null,
             'name' => null,
             'email' => null,
         ]);
     }
-
+    
     /**
-     * {@inheritdoc}.
+     * Get the code from the request.
+     *
+     * @return string
      */
-    protected function getTokenFields($code)
+    protected function getCode()
     {
-        return array_merge(parent::getTokenFields($code), [
-            'grant_type' => 'authorization_code',
-        ]);
+        return $this->request->input('auth_code');
     }
 
     /**
      * {@inheritdoc}.
      */
-    public function getAccessToken($code)
+    public function getAccessTokenResponse($code)
     {
-        $response = $this->getHttpClient()->post($this->getTokenUrl(), [
-            'query' => $this->getTokenFields($code),
-        ]);
-        return $this->parseAccessToken($response->getBody()->getContents());
+        $AlipaySystemOauthTokenRequest = new AlipaySystemOauthTokenRequest ();
+        $AlipaySystemOauthTokenRequest->setCode($code);
+        $AlipaySystemOauthTokenRequest->setGrantType("authorization_code");
+        $result = $this->executeAopClient($AlipaySystemOauthTokenRequest, $code);
+
+        $this->credentialsResponseBody = objectToArray($result->alipay_system_oauth_token_response);
+        $this->userId = $this->credentialsResponseBody['user_id'];
+
+        return $this->credentialsResponseBody;
     }
 
-    protected function getPortraitUrl($portrait)
+    /**
+     * 执行AopClient请求
+     * @param $request
+     * @param $token
+     * @return bool|mixed|\SimpleXMLElement
+     */
+    private function executeAopClient($request, $token)
     {
-        return 'https://openapi.alipay.com/gateway.do' . $portrait;
+        $aop = new AopClient ();
+        $aop->gatewayUrl = $this->getTokenUrl();
+        $aop->appId = $this->clientId;
+
+//        $aop->rsaPrivateKey = Storage::disk('alikey')->get('privateKey.rsa');
+//        $aop->alipayrsaPublicKey = Storage::disk('alikey')->get('publicKey.rsa');
+        $aop->rsaPrivateKeyFilePath = storage_path(config('services.alipay.privateKeyFilePath'));
+        $aop->alipayPublicKey = storage_path(config('services.alipay.publicKeyFilePath'));
+
+        $aop->signType = 'RSA2';
+        $aop->apiVersion = '1.0';
+        $result = $aop->execute($request, $token);
+
+        return $result;
+    }
+}
+
+/**
+ * 将对象转换为多维数组
+ *
+ **/
+function objectToArray($d)
+{
+    if (is_object($d)) {
+        // Gets the properties of the given object
+        // with get_object_vars function
+        $d = get_object_vars($d);
+    }
+
+    if (is_array($d)) {
+        /*
+        * Return array converted to object
+        * Using __FUNCTION__ (Magic constant)
+        * for recursive call
+        */
+        return array_map(__FUNCTION__, $d);
+    } else {
+        // Return array
+        return $d;
     }
 }
